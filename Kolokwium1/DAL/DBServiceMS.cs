58c25e2b384f714d2cfc2a01471be579189ead23
Julian Mikołajczyk <s18734@pjwstk.edu.pl>
2020-04-26 19:00:48 +0200

    Dodaj pliki projektów.

---
+++ Kolokwium1/DAL/DBServiceMS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolokwium1.DAL
+{
+    public class DBServiceMS : IDBService
+    {
+
+    }
+}
